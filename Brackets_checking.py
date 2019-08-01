570a214ff4c91cadad9fb171d09bb13ddf957b31
Leo <37282804+niuniu6niuniu@users.noreply.github.com>
2019-08-01 15:09:54 +0800

    Update Brackets_checking.py

--- Brackets_checking.py
+++ Brackets_checking.py
@@ -4,6 +4,9 @@
 # "(((())))", "()()()" is True
 # "(()))", ")))(((" is False
 
+# Idea: 1. Odd length string -> False
+#       2. Recursively remove "()", if sub-str left -> False
+
 def brackets(str):
     if len(str) % 2 == 1:
         return False
